15a16a07539ffcdb6b10e8722aa14849b02c4889
Salguero Mateu <fsalguer@t-systems.es>
2019-05-07 10:24:37 +0200

    versión recursiva

--- VBTraining/Ejercicio21.vb
+++ VBTraining/Ejercicio21.vb
@@ -7,13 +7,25 @@
         Console.Write("Número:")
         num = CInt(Console.ReadLine())
 
-        For i As Integer = 0 To num - 1
-            suma = suma + i
+        'For i As Integer = 0 To num - 1
+        '    suma = suma + i
 
-            Console.Write("La suma de los números que anteceden a el {0} es {1}\n\n", num, suma)
-        Next
+
+        'Next
+
+        'Console.Write("La suma de los números que anteceden a el {0} es {1}", num, suma)
+
+        Console.WriteLine($"La suma de los números que anteceden a el {num} es {Sumar(num - 1)}")
 
     End Sub
+
+    Function Sumar(ByVal num As Integer) As Integer
+        If (num > 0) Then
+            Return Sumar(num - 1) + num
+        End If
+
+        Return 0
+    End Function
 
 
 End Module
